a65ae1a057ad2a3dbc0e5baf0fd2b1f28fd7368b
nataliasdj <nataliasudjana@gmail.com>
2022-02-26 12:21:11 -0800

    deleted some previous commented code

--- visualize.py
+++ visualize.py
@@ -140,54 +140,7 @@ def astar_algorithm(draw, grid, start, end):
         if current != start:
             current.update_visited()
 
-    return print('No Path')
-
-# def astar_algorithm(draw, grid, start, end):
-# 	count = 0
-# 	open_set = PriorityQueue()
-# 	open_set.put((0, count, start))
-# 	came_from = {}
-# 	g_score = {spot: float("inf") for row in grid for spot in row}
-# 	g_score[start] = 0
-# 	f_score = {spot: float("inf") for row in grid for spot in row}
-# 	f_score[start] = h(start.get_pos(), end.get_pos())
-
-# 	open_set_hash = {start}
-
-# 	while not open_set.empty():
-# 		for event in pygame.event.get():
-# 			if event.type == pygame.QUIT:
-# 				pygame.quit()
-
-# 		current = open_set.get()[2]
-# 		open_set_hash.remove(current)
-
-# 		if current == end:
-# 			reconstruct_path(came_from, end, draw)
-# 			end.update_end_here()
-# 			return True
-
-# 		for neighbor in current.neighbors:
-# 			temp_g_score = g_score[current] + 1
-
-# 			if temp_g_score < g_score[neighbor]:
-# 				came_from[neighbor] = current
-# 				g_score[neighbor] = temp_g_score
-# 				f_score[neighbor] = temp_g_score + h(neighbor.get_pos(), end.get_pos())
-# 				if neighbor not in open_set_hash:
-# 					count += 1
-# 					open_set.put((f_score[neighbor], count, neighbor))
-# 					open_set_hash.add(neighbor)
-# 					neighbor.update_in_queue()
-
-# 		draw()
-
-# 		if current != start:
-# 			current.update_visited()
-
-# 	return False
-
-
+    return False
 
 #what data structure to hold all the nodes to make the grid, to traverse the nodes --> a list, not a 2d array
 #grid generation
