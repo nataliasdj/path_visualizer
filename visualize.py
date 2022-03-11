befb9e3262f7f72df255dc9143bb25ed6ca9d68b
nataliasdj <95986632+nataliasdj@users.noreply.github.com>
2022-03-10 22:48:11 -0800

    touch up

--- visualize.py
+++ visualize.py
@@ -90,7 +90,6 @@ def reconstruct_path(came_from, current, draw):
         current.update_path()
         draw()
 
-
 def astar_algorithm(draw, grid, start, end):
     #draw() is called
     count = 0 #keep track of when we insert item to queue to break ties if f score same
@@ -183,9 +182,6 @@ def get_clicked_pos(pos, rows, width):
     col = x // gap
     return row, col
 
-
-
-
 #collision checks, using the methods
 def main(win, width):
     ROWS = 50
@@ -250,63 +246,6 @@ def main(win, width):
     pygame.quit()
 
 main(WIN, WIDTH)
-
-
-# def main(win, width):
-# 	ROWS = 50
-# 	grid = make_grid(ROWS, width)
-
-# 	start = None
-# 	end = None
-
-# 	run = True
-# 	while run:
-# 		draw(win, grid, ROWS, width)
-# 		for event in pygame.event.get():
-# 			if event.type == pygame.QUIT:
-# 				run = False
-
-# 			if pygame.mouse.get_pressed()[0]: # LEFT
-# 				pos = pygame.mouse.get_pos()
-# 				row, col = get_clicked_pos(pos, ROWS, width)
-# 				spot = grid[row][col]
-# 				if not start and spot != end:
-# 					start = spot
-# 					start.update_start_here()
-
-# 				elif not end and spot != start:
-# 					end = spot
-# 					end.update_end_here()
-
-# 				elif spot != end and spot != start:
-# 					spot.update_barrier()
-
-# 			elif pygame.mouse.get_pressed()[2]: # RIGHT
-# 				pos = pygame.mouse.get_pos()
-# 				row, col = get_clicked_pos(pos, ROWS, width)
-# 				spot = grid[row][col]
-# 				spot.reset()
-# 				if spot == start:
-# 					start = None
-# 				elif spot == end:
-# 					end = None
-
-# 			if event.type == pygame.KEYDOWN:
-# 				if event.key == pygame.K_SPACE and start and end:
-# 					for row in grid:
-# 						for spot in row:
-# 							spot.update_neighbors(grid)
-
-# 					astar_algorithm(lambda: draw(win, grid, ROWS, width), grid, start, end)
-
-# 				if event.key == pygame.K_c:
-# 					start = None
-# 					end = None
-# 					grid = make_grid(ROWS, width)
-
-# 	pygame.quit()
-
-# main(WIN, WIDTH)
 
 
 
